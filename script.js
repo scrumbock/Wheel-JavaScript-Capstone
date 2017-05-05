02a6e005f8c726bd68f9e999f21cdce61887b706
scrumbock <scrumboc@yahoo.com>
2017-05-05 17:04:22 -0400

    finished additional category buttons

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
 var hangman = new HangMan();
 
-
-
 function HangMan() {
 	this.addWord = addWord;
 
@@ -11,6 +9,7 @@ function HangMan() {
 
 	var citiesArray = ["Wellington", "Auckland", "Christchurch"];
 	var bandsArray = ["Jesus Lizard", "Death Cab for Cutie", "Yes"]
+	var iceCreamArray = ["Butter Brickle", "Mint Chocolate Chip", "Rum Raisin"];
 
 	var solvedIndices = [];
 	var numTries = 3;
@@ -18,19 +17,22 @@ function HangMan() {
 	var input = document.getElementsByClassName("input-field")[0];
 	var submit = document.getElementsByClassName("submit")[0];
 	var ncb = document.getElementsByClassName("new-challenge-button-nz-cities")[0];
+	var bcb = document.getElementsByClassName("new-challenge-button-bands")[0];
+	var icb = document.getElementsByClassName("new-challenge-button-ice-cream")[0];
+
 	var tileCont = document.getElementsByClassName("tile-container")[0];
 
-	ncb.addEventListener("click", startChallenge)
+	ncb.addEventListener("click", startNzChallenge)
+	bcb.addEventListener("click", startBdChallenge)
+	icb.addEventListener("click", startIcChallenge)
+
 	submit.addEventListener("click", function() {
 		checkField(input.value);
 		input.value = "";
-
 	});
 
 
-
-	function startChallenge() {	
-		currentArray = 
+	function startNzChallenge() {	
 		currentWord = citiesArray[currentChallenge];
 		solvedIndices = [];
 		currentChallenge += 1;
@@ -39,6 +41,28 @@ function HangMan() {
 		}
 		displayWord();
 	}
+
+
+	function startBdChallenge() {	
+		currentWord = bandsArray[currentChallenge];
+		solvedIndices = [];
+		currentChallenge += 1;
+		if (currentChallenge === citiesArray.length) {
+			currentChallenge = 0;
+		}
+		displayWord();
+	}
+
+	function startIcChallenge() {	
+		currentWord = iceCreamArray[currentChallenge];
+		solvedIndices = [];
+		currentChallenge += 1;
+		if (currentChallenge === iceCreamArray.length) {
+			currentChallenge = 0;
+		}
+		displayWord();
+	}
+	
 
 	function addWord(word) {
 		citiesArray.push(word);
@@ -72,13 +96,22 @@ function HangMan() {
 			newDiv.classList.add("tile");
 
 			if (solvedIndices.indexOf(i) > -1) {
-				
 				newDiv.innerHTML = currentWord[i];
+
 			}
+
+			if (solvedIndices.indexOf(i) === -1) {
+				console.log([i]);
+				newDiv.innerHTML = " - ";
+			}	
+
 			tileCont.appendChild(newDiv);
 
 		};
-
+			
+			// else if (solvedIndices.indexOf(i) === -1) {
+			// 	newDiv.classList.add("black-tile");
+			// }	
 	}
 
 }
